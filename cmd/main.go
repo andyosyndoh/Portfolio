578c5af6d3cc666224035e831d61822bfd2279a7
aosindo <andyovvo8@gmail.com>
2024-10-20 00:35:07 +0300

    chore: added backend

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"portfolio/internals/routes"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(mux)
+
+	wrappedMux := routes.RouteChecker(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: wrappedMux,
+	}
+
+	fmt.Println("server running @http://localhost:8080\n=====================================")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("web error")
+		return
+	}
+}
